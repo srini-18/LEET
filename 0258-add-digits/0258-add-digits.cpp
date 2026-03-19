b2beabaeb70855e60688f74868bc38eda5b393b5
srini-18 <werewolfffyt@gmail.com>
2026-03-19 09:25:38 +0530

    Time: 0 ms (100%), Space: 8.5 MB (78.22%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while(num/10 > 0){
-            num= num%10 +num/10;
+        while(num/10>0){
+            num=num%10 + num/10;
         }
         return num;
     }
