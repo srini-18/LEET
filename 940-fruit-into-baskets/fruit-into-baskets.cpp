121595d81305ffc7e3f02e15cda50dac8dc22b7c
srini-18 <werewolfffyt@gmail.com>
2026-08-25 21:02:03 +0530

    Time: 51 ms (27.75%) | Memory: 84.8 MB (83.89%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int start=0;int maxlen=0;
+
+        unordered_map<int,int>basket;
+
+        for(int end=0;end<fruits.size();++end){
+            basket[fruits[end]]++;
+
+            while(basket.size()>2){
+                basket[fruits[start]]--;
+                if(basket[fruits[start]]==0){
+                    basket.erase(fruits[start]);
+                }
+            start++;
+            }
+        maxlen=max(maxlen,end-start+1);
+    }
+    return maxlen;
+    }
+};
